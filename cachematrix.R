6be94d6b63907976ed326e43532b31f5e8524700
unknown <onad@found-lon-wk26.itlab.local>
2016-02-05 16:14:08 +0000

    Solution
    This is my solution for Week 3 Programming Assignment2: Lexical Scoping
    for the R Programming course on Coursera.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Calculation of inverse matrix with a help of the cashing
+## to avoid unneccessary time and resource consumption.
 
-## Write a short comment describing this function
+## Function creates a list of functions to perform
+## matrix inversion: 
+## 1. set - sets the value of the matrix
+## 2. get - gets the value of the matrix
+## 3. setInverse - sets the inverse of the matrix
+## 4. getInverse - gets the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv_matrix <- NULL
+  set <-function(y) {
+  	x <<- y
+  	inv_matrix <<- NULL
+  }
+  get <-function() {
+  	x
+  }
+  setInverse <- function(inverse) inv_matrix <<- inverse
+  getInverse <- function() inv_matrix
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## This function returns the inverse of the matrix.
+## If inversion has been computed gets the result 
+## from the cashe, in another case it makes a calculation.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if (is.null(inv)) {
+  	my_matrix <- x$get()
+  	inv <- solve(data)
+  	x$setInverse(inv)
+  }
+  ## Return a matrix that is the inverse of 'x'
+  inv     
 }
